5c9e5990d4f2f4492a885ffefda9edb21646b48d
David Mena Velasquez <dmenav@unal.edu.co>
2022-10-30 19:04:00 -0500

    Initial commit

---
+++ src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main (String [] args){
+
+        int n= 1234567890;
+        long nL= 1234567891234L;
+
+        double nD= 3.14516;
+        float nf= 123.6287837327F;
+
+        var salary= 1000;
+        var pension = salary*0.03;
+        var totalSalary= salary-pension;
+        var employeeName= "David Mena";
+        System.out.print(salary);
+        System.out.print(pension);
+        System.out.print(totalSalary);
+        System.out.print("Empleado: "+ employeeName + "Salary: "+ totalSalary);
+
+
+
+
+    }
+}
